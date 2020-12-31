e27e17a1a3c519f716cdf02eedcffdc7969c88bc
takanakahiko <takanakahiko@gmail.com>
2020-12-31 13:16:01 +0900

    fix process for send messages

--- src/popup.ts
+++ src/popup.ts
@@ -5,24 +5,17 @@ const tweetButton = document.getElementById('tweet') as HTMLAnchorElement
 
 analyzeButton.addEventListener('click', function () {
     this.textContent = 'Analyzing...'
-    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-        console.log(tabs)
-        if (tabs.length != 0) {
-            chrome.tabs.sendMessage(tabs[0].id as number, 'analyze', result => {
-                if (result != null) {
-                    const purchaseResult = result as PurchaseResult
-                    this.style.display = 'none'
-                    numbers.textContent = `2020年の購入作品数: ${purchaseResult.worksCount}`
-                    price.textContent = `総額: ${purchaseResult.totalPrice}円`
-                    const tweetContent = `私のDLsite散財2020\n購入作品数: ${purchaseResult.worksCount}作品\n合計金額: ${purchaseResult.totalPrice}円\n`
-                    tweetButton.style.display = 'block'
-                    const gitHubUrl = 'https://github.com/yt8492/DLsiteSanzai2020'
+    chrome.runtime.sendMessage('analyze', result => {
+        if (result != null) {
+            const purchaseResult = result as PurchaseResult
+            this.style.display = 'none'
+            numbers.textContent = `2020年の購入作品数: ${purchaseResult.worksCount}`
+            price.textContent = `総額: ${purchaseResult.totalPrice}円`
+            const tweetContent = `私のDLsite散財2020\n購入作品数: ${purchaseResult.worksCount}作品\n合計金額: ${purchaseResult.totalPrice}円\n`
+            tweetButton.style.display = 'block'
+            const gitHubUrl = 'https://github.com/yt8492/DLsiteSanzai2020'
 
-                    tweetButton.href = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetContent)}&hashtags=${encodeURIComponent('DLsite散財2020')}&url=${encodeURIComponent(gitHubUrl)}`
-                } else {
-                    this.textContent = '失敗しました。ページをリロードして再試行してください。'
-                }
-            })
+            tweetButton.href = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetContent)}&hashtags=${encodeURIComponent('DLsite散財2020')}&url=${encodeURIComponent(gitHubUrl)}`
         } else {
             this.textContent = '失敗しました。ページをリロードして再試行してください。'
         }
